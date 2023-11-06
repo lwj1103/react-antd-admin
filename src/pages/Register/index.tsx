6927a5ad248fdbf80908361df461cfe32ebde4e4
lwj1103 <2744769805@qq.com>
2023-11-06 17:02:37 +0800

    gggg

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,126 @@
+import type { RegisterParams } from '@/interface/user/register';
+import type { FC } from 'react';
+
+import './index.less';
+
+import { Button, Checkbox, Form, Input } from 'antd';
+import { useDispatch } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import { LocaleFormatter, useLocale } from '@/locales';
+import { formatSearch } from '@/utils/formatSearch';
+
+import { RegisterAsync } from '../../stores/user.action';
+
+import BackgroundImage from './RE4wppI.jpg'
+
+const initialValues: RegisterParams = {
+  username: 'guest',
+  password: 'guest',
+  // remember: true
+};
+
+const RegisterForm: FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const { formatMessage } = useLocale();
+
+  const onFinished = async (form: RegisterParams) => {
+    const res = dispatch(await RegisterAsync(form));
+
+    if (!!res) {
+      const search = formatSearch(location.search);
+      const from = search.from || { pathname: '/' };
+
+      navigate(from);
+    }
+  };
+
+  return (
+    // style={{backgroundImage :`url(${BackgroundImage})`,  backgroundSize : 'cover'}}
+    <div className="Register-page">
+      <Form<RegisterParams> onFinish={onFinished} className="Register-page-form" initialValues={initialValues}>
+        <h2>CONTRACT MANAGE</h2>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: formatMessage({
+                id: 'gloabal.tips.enterUsernameMessage',
+              }),
+            },
+          ]}
+        >
+          <Input
+            placeholder={formatMessage({
+              id: 'gloabal.tips.username',
+            })}
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: formatMessage({
+                id: 'gloabal.tips.enterPasswordMessage',
+              }),
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder={formatMessage({
+              id: 'gloabal.tips.password',
+            })}
+          />
+        </Form.Item>
+        <Form.Item
+          name="confirm_password"
+          rules={[
+            {
+              required: true,
+              message: formatMessage({
+                id: 'gloabal.tips.enterPasswordMessage',
+              }),
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder={formatMessage({
+              id: 'gloabal.tips.password',
+            })}
+          />
+        </Form.Item>
+        <Form.Item
+          name="get_code"
+          rules={[
+            {
+              required: true,
+              message: formatMessage({
+                id: 'gloabal.tips.enterPasswordMessage',
+              }),
+            },
+          ]}
+        >
+          <Input
+            type="password"
+            placeholder={formatMessage({
+              id: 'gloabal.tips.password',
+            })}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit" type="primary" className="login-page-form_button">
+            <LocaleFormatter id="gloabal.tips.create" />
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default RegisterForm;
